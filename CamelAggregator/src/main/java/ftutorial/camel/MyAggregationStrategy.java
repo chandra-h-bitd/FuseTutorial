96a5938a52922d00dde145e20338483b765ea99c
Chandrakant Hardahe <chardahe@chardahe.pnq.csb>
2019-01-03 12:55:35 +0530

    Add Camel Aggregator

---
+++ CamelAggregator/src/main/java/ftutorial/camel/MyAggregationStrategy.java
@@ -0,0 +1,26 @@
+package ftutorial.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+public class MyAggregationStrategy implements AggregationStrategy {
+
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+    	
+        if (oldExchange == null) {
+            return newExchange;
+        }
+
+        String input2 = newExchange.getIn().getBody(String.class);
+        if ("STOP".equalsIgnoreCase(input2)) {
+            return oldExchange;
+        }
+        String input1 = oldExchange.getIn().getBody(String.class);
+
+
+        String output = input1+" "+input2;
+        oldExchange.getIn().setBody(output);
+
+        return oldExchange;
+    }
+}
