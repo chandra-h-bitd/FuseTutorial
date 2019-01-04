65ae94f41ad9ed42b891ef8844479edc188fa2fc
Chandrakant Hardahe <chardahe@chardahe.pnq.csb>
2019-01-04 13:54:13 +0530

    adding files

---
+++ CamelEnrich/src/main/java/ftutorial/camel/MyAggregationStrategy.java
@@ -0,0 +1,21 @@
+package ftutorial.camel;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+public class MyAggregationStrategy implements AggregationStrategy {
+
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+    	
+        if (oldExchange == null) {
+            return newExchange;
+        }
+        String input2 = newExchange.getIn().getBody(String.class);
+        String input1 = oldExchange.getIn().getBody(String.class);
+
+        String output = input1+" "+input2;
+        oldExchange.getIn().setBody(output);
+
+        return oldExchange;
+    }
+}
